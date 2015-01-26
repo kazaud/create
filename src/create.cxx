dfc02022b48df166e480aaf37b3596e8ac06f07b
Jonathan Koberstein <jkobe@localhost>
2015-01-25 16:38:58 -0800

    Ignoring build folderwq

--- src/create.cxx
+++ src/create.cxx
@@ -9,6 +9,6 @@ int main (int argc, char *argv[]) {
   }
   double inputValue = atof(argv[1]);
   double outputValue = sqrt(inputValue);
-  fprintf(stdout,"The square root of %g is %g\n", inputValue, outputValue);
+  fprintf(stdout,"The sqrt of %g is %g\n", inputValue, outputValue);
   return 0;
 }
