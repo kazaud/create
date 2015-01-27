28da95227f9450cc58b68504c9c3bd1b8863a472
Jonathan Koberstein <jkobe@localhost>
2015-01-26 17:05:26 -0800

    setting up folder structure

--- skeleton/src/create.cxx
+++ skeleton/src/create.cxx
@@ -1,14 +1,16 @@
 #include <cstdio>
 #include <cstdlib>
 #include <cmath>
+#include "create_config.h"
 
 int main (int argc, char *argv[]) {
+  fprintf(stdout, "Create version %d,%d\n", kCreateVersionMajor, kCreateVersionMinor);
   if (argc < 2) {
     fprintf(stdout,"Usage: %s number\n",argv[0]);
     return 1;
   }
   double inputValue = atof(argv[1]);
   double outputValue = sqrt(inputValue);
-  fprintf(stdout,"The sqrt of %g is %g\n", inputValue, outputValue);
+  fprintf(stdout,"The sqrxxxxxxxxxxxt of %g is %g\n", inputValue, outputValue);
   return 0;
 }
