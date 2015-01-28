554415203fc1538dc711055c3d1650a8c5f4dc6e
Jonathan Koberstein <jkobe@localhost>
2015-01-27 23:48:30 -0800

    Removing debug echo in clean script.

--- clean.sh
+++ clean.sh
@@ -2,5 +2,4 @@ files="$files $(find . -name "CMakeCache.txt" -perm -u+w)"
 files="$files $(find . -name "*.cmake" -perm -u+w)"
 files="$files $(find . -name "Makefile" -perm -u+w)"
 files="$files $(find . -name "CMakeFiles" -perm -u+w)"
-echo $files
 rm -r $files
