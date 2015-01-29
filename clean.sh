9b6e55b797d502c7afcd1eeffd671480ed67fdf3
Jonathan Koberstein <jkobe@localhost>
2015-01-28 17:29:26 -0800

    Fixing c++11 configuration to work and and ninja files to clean script.

--- clean.sh
+++ clean.sh
@@ -2,4 +2,8 @@ files="$files $(find . -name "CMakeCache.txt" -perm -u+w)"
 files="$files $(find . -name "*.cmake" -perm -u+w)"
 files="$files $(find . -name "Makefile" -perm -u+w)"
 files="$files $(find . -name "CMakeFiles" -perm -u+w)"
+files="$files $(find . -name "*.ninja" -perm -u+w)"
+files="$files $(find . -name ".ninja_deps" -perm -u+w)"
+files="$files $(find . -name ".ninja_log" -perm -u+w)"
 rm -r $files
+exit 0
