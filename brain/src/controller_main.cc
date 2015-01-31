594b1563f6ea65be73082a65e2cd74a8ed3b0383
Jonathan Koberstein <jkobe@localhost>
2015-01-31 14:28:05 -0800

    Starting demo mode

--- brain/src/controller_main.cc
+++ brain/src/controller_main.cc
@@ -16,8 +16,12 @@ int main(int argc, char* argv[]) {
     fprintf(stdout, "Usage: %s serial port. I.e. /dev/ttyUSB0\n", argv[0]);
     return 1;
   }
-  SerialStream stream("/dev/ttyS0");
-  Create robot(stream);
-  robot.sendDemoCommand(Create::DEMO_COVER);
+  SerialStream stream(argv[1], SerialStreamBuf::BAUD_57600);
+  stream << static_cast<char>(128);
+  stream << static_cast<char>(136);
+  stream << static_cast<char>(0);
+
+  //Create robot(stream);
+  //robot.sendDemoCommand(Create::DEMO_COVER);
   return 0;
 }
